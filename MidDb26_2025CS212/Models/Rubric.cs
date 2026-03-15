90ac3dd85235b8a3bf243b0774243ee829db22d5
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 18:10:13 +0500

    Added Modal Class - Rubric.cs

---
+++ MidDb26_2025CS212/Models/Rubric.cs
@@ -0,0 +1,12 @@
+﻿namespace MidDb26_2025CS212.Models
+{
+    public class Rubric
+    {
+        public int Id { get; set; }
+        public string Details { get; set; }
+        public int CloId { get; set; }
+        public string CloName { get; set; }
+
+        public override string ToString() => Details;
+    }
+}
