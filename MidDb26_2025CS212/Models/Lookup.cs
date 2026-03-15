6b48ebe55352c881641271ef82e9baba1b3cbf03
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 20:06:00 +0500

    Added Model Class - Lookup.cs

---
+++ MidDb26_2025CS212/Models/Lookup.cs
@@ -0,0 +1,11 @@
+﻿namespace MidDb26_2025CS212.Models
+{
+    public class Lookup
+    {
+        public int LookupId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
