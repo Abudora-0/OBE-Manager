0de0d85e17eefc444a7bd2e7f22174934cb6bfa4
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 18:55:11 +0500

    Added Modal Class - Assessment.cs

---
+++ MidDb26_2025CS212/Models/Assessment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MidDb26_2025CS212.Models
+{
+    public class Assessment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int TotalMarks { get; set; }
+        public int TotalWeightage { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
