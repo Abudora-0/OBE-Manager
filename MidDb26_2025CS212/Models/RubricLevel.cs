c9a1e7a0ab6cd1ceb1610a682081c900473ec301
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 18:36:43 +0500

    Added Modal Class - RubricLevel.cs

---
+++ MidDb26_2025CS212/Models/RubricLevel.cs
@@ -0,0 +1,13 @@
+﻿namespace MidDb26_2025CS212.Models
+{
+    public class RubricLevel
+    {
+        public int Id { get; set; }
+        public int RubricId { get; set; }
+        public string Details { get; set; }
+        public int MeasurementLevel { get; set; }
+
+        public override string ToString() =>
+            "Level " + MeasurementLevel + " - " + Details;
+    }
+}
