064721f5c1fe60a298157b5a950e6b24a9593b7b
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 19:23:47 +0500

    Added Modal Class - AssessmentComponent.cs

---
+++ MidDb26_2025CS212/Models/AssessmentComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MidDb26_2025CS212.Models
+{
+    public class AssessmentComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RubricId { get; set; }
+        public string RubricName { get; set; }
+        public int TotalMarks { get; set; }
+        public int AssessmentId { get; set; }
+        public string AssessmentName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
