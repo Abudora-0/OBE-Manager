309fbe4ac7b185a84caffead51244d18de243b4c
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 17:42:31 +0500

    Added Model Class - Student.cs

---
+++ MidDb26_2025CS212/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace MidDb26_2025CS212.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int Status { get; set; }
+        public string StatusName { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+
+        public override string ToString() => FullName;
+    }
+}
