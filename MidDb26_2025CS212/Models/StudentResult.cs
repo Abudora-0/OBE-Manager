e0065f5c9ac0974f45630e2dc3b4357fdf078410
Abudora-0 <m.abdullah21306@gmail.com>
2026-03-15 19:44:02 +0500

    Added Modal Class - StudentResult.cs

---
+++ MidDb26_2025CS212/Models/StudentResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MidDb26_2025CS212.Models
+{
+    public class StudentResult
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int AssessmentComponentId { get; set; }
+        public string ComponentName { get; set; }
+        public int RubricMeasurementId { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public int ObtainedLevel { get; set; }
+        public int MaxLevel { get; set; }
+        public decimal ComponentMarks { get; set; }
+
+        public decimal ObtainedMarks =>
+            MaxLevel == 0 ? 0 :
+            ((decimal)ObtainedLevel / MaxLevel) * ComponentMarks;
+    }
+}
